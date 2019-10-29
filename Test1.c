512f5465b1cf01693ce018dfbebc2854632560b8
maria-nazarova <55286540+maria-nazarova@users.noreply.github.com>
2019-10-29 19:22:33 +0300

    Add files via upload

--- Test1.c
+++ Test1.c
@@ -3,18 +3,20 @@
 
 size_t maxSize = 250;
 
-int main(){
+int main() {
 	int n;
-	unsigned int f[maxSize];
-	f[0] = 1;
-	f[1] = 1;
+	unsigned int fib0, fib1, fib2;
+	fib0 = 1;
+	fib1 = 1;
 	scanf("%d", &n);
 	if (n < 0) {
 		printf("Incorrect value");
 	} else {
-		for (int i = 2; i <= n; i++){
-			f[i] = f[i - 1] + f[i - 2];
+		for (int i = 2; i <= n; i++) {
+			fib2 = fib1 + fib0;
+			fib0 = fib1;
+			fib1 = fib2;
 		}
-		printf("%u", f[n]);
+		printf("%lu", fib1);
 	}
 }
