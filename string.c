6373bccfd951680c2f1107369494e4f0af254abe
maria-nazarova <55286540+maria-nazarova@users.noreply.github.com>
2019-11-19 17:09:24 +0300

    Update string.c

--- string.c
+++ string.c
@@ -23,7 +23,7 @@ void strcat_(char *dst, char *str) {
 }
 
 int strcmp_(char *str1, char *str2) {
-	size_t len1 = strlen_(str1), len2 = strlen_(str2), i = 0;
+	size_t i = 0;
 	do {
 		if (str1[i] > str2[i]) {
 			return 1;
@@ -32,13 +32,7 @@ int strcmp_(char *str1, char *str2) {
 			return -1;
 		}
 	} while (str2[i++]);
-	if (len1 > len2) {
-		return 1;
-	} else if (len2 > len1) {
-		return -1;
-	} else {
 		return 0;
-	}
 }
 
 size_t maxLen = 300;
